b62499f347bc65b595f6587bb8c1ff442ed7441d
Jonah <jonah@Madisons-MBP-2.lan>
2020-04-07 23:08:25 -0600

    Add new code challenge

---
+++ CodeChallenges.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ string:String) -> Int {
+    var sum = 0
+    var romans = [
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    ]
+    
+    for value in romans.values {
+        
+    }
+    return sum
+}
+//Symbol    Value
+//I          1
+//V          5
+//X          10
+//L          50
+//C          100
+//D          500
+//M          1,000
+
+//: [Next](@next)
